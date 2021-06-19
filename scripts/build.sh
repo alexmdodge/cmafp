cfa720c2df28afe6c27d09e216cae97c56b403d4
Alex Dodge <alex.dodge@redspace.com>
2021-06-19 11:32:44 -0300

    chore: consolidate build path entries
    Template build and lib metadata.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
-source ./scripts/utils.sh
 
 SCRIPT_DIR="$( cd "$( dirname "$BASH_SOURCE0]}" )" &> /dev/null && pwd )"
+source "${SCRIPT_DIR}/utils.sh"
 
 REPO_ROOT="$SCRIPT_DIR/.."
+OUTPUT_VAL="console.log(require('${REPO_ROOT}/package.json').config.cmafp"
 
-DIR_BASE=$(node -e "require('${REPO_ROOT}/package.json').config.cmafp.libDir")
-LIB_NAME=$(node -e "require('${REPO_ROOT}/package.json').config.cmafp.lib")
-LIB_DIR="$SCRIPT_DIR/${DIR_BASE}"
+DIR_BASE=$(node -e "${OUTPUT_VAL}.libDir)")
+LIB_NAME=$(node -e "${OUTPUT_VAL}.lib)")
+LIB_DIR="$REPO_ROOT/${DIR_BASE}"
 
-CPP_BASE=$(node -e "require('${REPO_ROOT}/package.json').config.cmafp.intput")
-WASM_BASE=$(node -e "require('${REPO_ROOT}/package.json').config.cmafp.output")
-WASM_OUTPUT="${SCRIPT_DIR}/${WASM_BASE}"
+echo "LIB DIR ${LIB_DIR}"
 
-# Clean previous build
-#rm -rf $LIB_DIR
-#rm $LIB_OUTPUT
+CPP_BASE=$(node -e "${OUTPUT_VAL}.input)")
+WASM_BASE=$(node -e "${OUTPUT_VAL}.output)")
+WASM_OUTPUT="${REPO_ROOT}/${WASM_BASE}"
 
 # Create the library directory and build using the cmake wrapper
 mkdir -p $LIB_DIR
 cd $LIB_DIR
+
 emcmake cmake $REPO_ROOT
 emmake make
 
 # Attach binds in static lib to output JS
-cd $SCRIPT_DIR
+cd $REPO_ROOT
 emcc \
   -Isrc/reader -Isrc/builder -Isrc/helpers -Isrc/parser -Isrc/boxes \
-  --bind $CPP_BASE $LIB_DIR/$LIB_BASE \
+  --bind $CPP_BASE $LIB_DIR/$LIB_NAME \
   -o $WASM_OUTPUT
