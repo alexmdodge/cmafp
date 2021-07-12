0d3ef168dfe3ef19077995efeff960e295956fcd
Alex Dodge <alex.mdodge@gmail.com>
2021-07-12 16:02:25 -0300

    feat: abstraction for FullBox

---
+++ src/boxes/FullBox.cpp
@@ -0,0 +1,20 @@
+#include "FullBox.h"
+
+#include "BinaryHelpers.h"
+
+FullBox::FullBox(std::shared_ptr<MediaFile>& file, uint32_t start_offset) : Box{file, start_offset} {
+    version = shift_u8();
+
+    flags += BinaryHelpers::uint8_to_hex_string(shift_u8());
+    flags += BinaryHelpers::uint8_to_hex_string(shift_u8(), false);
+    flags += BinaryHelpers::uint8_to_hex_string(shift_u8(), false);
+};
+
+json FullBox::to_json() {
+    json data{};
+
+    data["version"] = version;
+    data["flags"] = flags;
+
+    return data;
+}
---
+++ src/boxes/FullBox.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <string>
+
+#include "Box.h"
+#include "MediaFile.h"
+
+class FullBox : public Box {
+   public:
+    FullBox(std::shared_ptr<MediaFile>& file, uint32_t start_offset);
+
+    json to_json();
+
+    uint8_t version;
+    std::string flags;
+};
